1ed33f297f579556e3c7d3ee949c7b065ca91def
xmanuel150 <manuel.monzon.rojas@gmail.com>
2025-02-23 09:26:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html lang="es">
+
+</html>
+
+<head>
+    <title>Consultar BD AJAX</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link href="css/estilos.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  
+    <h4>hola, este es mi primera página</h4>
+
+
+    <footer>
+    </footer>
+    </body>
+
+    </html>
